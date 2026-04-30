f3d0c3fcb98510db9c48b9a75f1dffe6abb39c68
kavya-reddy13 <Kavyareddy1309@gmail.com>
2026-05-01 00:18:51 +0530

    Add files via upload

---
+++ Task2.py
@@ -0,0 +1,100 @@
+# 1. Area of Square
+# side = 5
+# area_square = side * side
+# print("Area of square is:", area_square)
+
+
+# 2. Area of Rectangle
+
+# length = 6
+# breadth = 4
+# area_rectangle = length * breadth
+# print("Area of rectangle is:", area_rectangle)
+
+
+# 3. Area of Triangle
+
+# base = 8
+# height = 5
+# area_triangle = 0.5 * base * height
+# print("Area of triangle is:", area_triangle)
+
+
+# 4. Perimeter of Square
+
+# side = 6
+# perimeter_square = 4 * side
+# print("Perimeter of square is:", perimeter_square)
+
+
+# 5. Perimeter of Rectangle
+
+# length = 5
+# breadth = 3
+# perimeter_rectangle = 2 * (length + breadth)
+# print("Perimeter of rectangle is:", perimeter_rectangle)
+
+
+# 6. Perimeter of Triangle
+
+# side1 = 5
+# side2 = 6
+# side3 = 7
+# perimeter_triangle = side1 + side2 + side3
+# print("Perimeter of triangle is:", perimeter_triangle)
+
+
+# 7. Break Amount into 1000s, 500s, and Remaining Change
+
+# amount = 3700
+# thousands = amount // 1000
+# remaining = amount % 1000
+# five_hundreds = remaining // 500
+# remaining = remaining % 500
+# print("1000s:", thousands)
+# print("500s:", five_hundreds)
+# print("Remaining:", remaining)
+
+
+# 8. Convert Seconds into Hours, Minutes, and Seconds
+
+# total_seconds = 3672
+# hours = total_seconds // 3600
+# remaining_seconds = total_seconds % 3600
+# minutes = remaining_seconds // 60
+# seconds = remaining_seconds % 60
+# print("Hours:", hours)
+# print("Minutes:", minutes)
+# print("Seconds:", seconds)
+
+
+# 9. Sum of Marks (Maths, Physics, Chemistry)
+
+# maths = 85
+# physics = 90
+# chemistry = 88
+# total_marks = maths + physics + chemistry
+# print("Total marks:", total_marks)
+
+
+# 10. Average of marks (maths, physics, chemistry)
+
+# maths = 85
+# physics = 90
+# chemistry = 88
+# total = maths + physics + chemistry
+# average = total / 3
+# print("Average marks:", round(average, 2))
+
+
+
+
+
+
+
+
+
+
+
+
+
